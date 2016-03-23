0e10d7ef69e2174a4b31cf5d73e42581e807531b
ronliu <ronliu2k@gmail.com>
2016-03-23 15:16:30 +1100

    finished introduce bootstrap

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var $= require('jquery');
+require('./style.scss');
 document.addEventListener('DOMContentLoaded', function() {
 	console.log('hi');
 	$('button').click(function(){alert('clicked1')});
--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,21 @@ module.exports = {
 	output: {
 		path: __dirname,
 		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.scss$/,
+				loader: 'style-loader!css-loader!sass-loader'
+			},
+			{
+				test: [/\.svg/, /\.eot/, /\.ttf/, /\.eot/],
+				loader: 'file-loader'
+			},
+			{
+				test: [/\.woff/, /\.woff2/],
+				loader: "url-loader?limit=100000&minetype=application/font-woff"
+			}
+		]
 	}
 };
