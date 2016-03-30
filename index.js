49fc95ae03763db27c803675a11adf6c057a0b6b
ronliu <ronliu2k@gmail.com>
2016-03-30 11:03:29 +1100

    show off es6 es7 grammar

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 //var $= require('jquery');
 //require('./style.scss');
 //	$('button').click(function(){alert('clicked1')});
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
 	console.log('hi');
+	let b = new B();
+	b.do();
+	let {name} = b;
+	console.log('b\'s name is', name);
+
+	let arr= [0,1,2,3,4];
+	console.log('arr is now', [...arr.slice(0,2), ...arr.slice(3)])
 });
 
-class A {}
+class A {
+	constructor() {
+		console.log('constructing');
+	}
+}
+
+class B extends A {
+	do() {
+		console.log('B is doing');
+	}
+	name= 'abc'
+}
