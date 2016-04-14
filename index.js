6d5809cd1b6aa9558c2708163c1d67902d5c0d82
ronliu <ronliu2k@gmail.com>
2016-04-14 11:55:43 +1000

    starting extract css

--- index.js
+++ index.js
@@ -1,26 +1,5 @@
-//var $= require('jquery');
-//require('./style.scss');
-//	$('button').click(function(){alert('clicked1')});
+var $= require('jquery');
+require('./style.scss');
 document.addEventListener('DOMContentLoaded', () => {
-	console.log('hi');
-	let b = new B();
-	b.do();
-	let {name} = b;
-	console.log('b\'s name is', name);
-
-	let arr= [0,1,2,3,4];
-	console.log('arr is now', [...arr.slice(0,2), ...arr.slice(3)])
+	$('button').click(function(){alert('clicked1')});
 });
-
-class A {
-	constructor() {
-		console.log('constructing');
-	}
-}
-
-class B extends A {
-	do() {
-		console.log('B is doing');
-	}
-	name= 'abc'
-}
