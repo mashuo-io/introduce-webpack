53fb60fadcd2a0aaa0b2d410325858cc5a9d7deb
ronliu <ronliu2k@gmail.com>
2016-04-14 11:55:57 +1000

    finished extracting

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
 	entry: './index.js',
 	output: {
 		path: __dirname,
 		filename: 'bundle.js'
 	},
+	plugins: [new ExtractTextPlugin('style.css')],
 	module: {
 		loaders: [
 			{
 				test: /\.scss$/,
-				loader: 'style-loader!css-loader!sass-loader'
+				loader: ExtractTextPlugin.extract('css-loader!sass-loader')
 			},
 			{
 				test: [/\.svg/, /\.eot/, /\.ttf/, /\.eot/],
