9e52613ff3908fc8b2c8352f57377f105a5c12cc
ronliu <ronliu2k@gmail.com>
2016-04-15 09:52:07 +1000

    finished

--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,18 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
+var config = {
 	entry: './index.js',
 	output: {
-		path: __dirname + '/dist',
+
 		filename: 'bundle.js'
 	},
 	plugins: [
-		new ExtractTextPlugin('style.css'),
-		new webpack.optimize.DedupePlugin(),
-		new webpack.optimize.UglifyJsPlugin(),
-		new HtmlWebpackPlugin({template: './index.html', inject: false})
+		new ExtractTextPlugin('style.css')
+
 	],
 	module: {
 		loaders: [
-			{
-				test: /\.scss$/,
-				loader: ExtractTextPlugin.extract('css!sass')
-			},
 			{
 				test: [/\.svg/, /\.eot/, /\.ttf/, /\.eot/],
 				loader: 'file'
@@ -35,3 +29,26 @@ module.exports = {
 		]
 	}
 };
+
+if (process.env.NODE_ENV === "production") {
+	config.output.path = __dirname + '/dist';
+	config.plugins = config.plugins.concat([
+		new webpack.optimize.DedupePlugin(),
+		new webpack.optimize.UglifyJsPlugin(),
+		new HtmlWebpackPlugin({template: './index.html', inject: false})
+	]);
+	config.module.loaders.push({
+		test: /\.scss$/,
+		loader: ExtractTextPlugin.extract('css!sass')
+	})
+} else {
+	config.output.path = __dirname;
+	config.devtool = "source-map";
+	config.module.loaders.push({
+		test: /\.scss$/,
+		loader: ExtractTextPlugin.extract('css?sourceMap!sass?sourceMap')
+	})
+
+}
+
+module.exports = config;
